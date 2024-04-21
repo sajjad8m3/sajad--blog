4bacf03e662e443578b27bf5973a95b774f8a105
sajadm83 <sajadmortazavi83@gmail.com>
2024-04-21 02:13:36 -0700

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
+import Dashbord from "./pages/Dashbord";
 
 const App = () => {
   return (
-    <h1 className='text-3xl bg-red-800'>sajjad</h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path='/dashbord' element={<Dashbord/>} />
 
-export default App
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
+export default App;
