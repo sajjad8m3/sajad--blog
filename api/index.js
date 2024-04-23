f5da7e9d2362fbb0a169e340482f3e3783163478
sajadm83 <sajadmortazavi83@gmail.com>
2024-04-23 03:12:54 -0700

    connect to the data base

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(
+    process.env.MONGO
+).then(
+    ()=>{
+        console.log('mogodb is connected');
+    }
+).catch(err =>{
+    console.log(err)
+})
 const app = express();
 app.listen(3000, () => {
   console.log("server is running prot 3000!");
