648736377013a6a962107c0a2b93cb9d7fd12202
sajadm83 <sajadmortazavi83@gmail.com>
2024-04-24 23:35:06 -0700

    creat a test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = ( req, res) =>{
+    res.json({massage: 'API is working'});
+};
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 mongoose.connect(
     process.env.MONGO
@@ -15,3 +16,4 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running prot 3000!");
 });
+app.use('/api/user', userRoutes);
