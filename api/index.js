f0ea742ee343399b6a917afca6e320603b12a3f6
sajadm83 <sajadmortazavi83@gmail.com>
2024-04-25 01:30:32 -0700

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(
     process.env.MONGO
@@ -11,9 +12,13 @@ mongoose.connect(
     }
 ).catch(err =>{
     console.log(err)
-})
+});
 const app = express();
+app.use(express.json());
+
+
 app.listen(3000, () => {
   console.log("server is running prot 3000!");
 });
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
